ab188be58bd0ef478cc45eaaef6290f18bcca59a
Dan Kang <dan@dskang.com>
2012-01-29 19:57:58 -0500

    Create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tigerpass::Application.config.secret_token = '4c547a5f81d5253331eb2c3f89fe8f0e9fdc7af385e0be9ee97912202b9b85c8a15f6049f250f132712fa191da86c891a05ff5febfba0f4ce5867dc41c3a1f70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tigerpass::Application.config.session_store :cookie_store, key: '_tigerpass_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tigerpass::Application.config.session_store :active_record_store
