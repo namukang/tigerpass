69f073dc51c588b9f9e9130d4e37ee1d082901af
Dan Kang <dan@dskang.com>
2012-02-03 16:17:20 -0500

    Seed database with clubs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Club.create(short_name: 'Terrace',
+            long_name: 'Terrace Club',
+            logo: 'terrace.png')
+Club.create(short_name: 'Tower',
+            long_name: 'Tower Club',
+            logo: 'tower.png')
+Club.create(short_name: 'Cannon',
+            long_name: 'Cannon Club',
+            logo: 'cannon.png')
+Club.create(short_name: 'Quad',
+            long_name: 'Quadrangle Club',
+            logo: 'quad.png')
+Club.create(short_name: 'Colonial',
+            long_name: 'Colonial Club',
+            logo: 'colonial.png')
+Club.create(short_name: 'Ivy',
+            long_name: 'Ivy Club',
+            logo: 'ivy.png')
+Club.create(short_name: 'TI',
+            long_name: 'Tiger Inn',
+            logo: 'ti.png')
+Club.create(short_name: 'Cottage',
+            long_name: 'Cottage Club',
+            logo: 'cottage.png')
+Club.create(short_name: 'Cap',
+            long_name: 'Cap and Gown',
+            logo: 'cap.png')
+Club.create(short_name: 'Cloister',
+            long_name: 'Cloister Inn',
+            logo: 'cloister.png')
+Club.create(short_name: 'Charter',
+            long_name: 'Charter Club',
+            logo: 'charter.png')
