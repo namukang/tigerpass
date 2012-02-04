5d3dc396cdb0edb4a4e2060cc706a909b162740d
Dan Kang <dan@dskang.com>
2012-02-04 03:06:46 -0500

    Clubs should only be viewable

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -14,7 +14,7 @@ class ClubsController < ApplicationController
         redirect_to @oauth.url_for_oauth_code(permissions: "email", redirect_uri: "http://localhost:3000/") and return
       end
     else
-      @graph = Koala::Facebook::API.new(session[:access_token]) 
+      @graph = Koala::Facebook::API.new(session[:access_token])
       @fb_user = @graph.get_object("me")["id"]
       @fb_id = @fb_user['id']
       @user = User.where(fb_id: @fb_id)
@@ -40,53 +40,6 @@ class ClubsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @club }
-    end
-  end
-
-  def new
-    @club = Club.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @club }
-    end
-  end
-
-  def edit
-  end
-
-  def create
-    @club = Club.new(params[:club])
-
-    respond_to do |format|
-      if @club.save
-        format.html { redirect_to @club, notice: 'Club was successfully created.' }
-        format.json { render json: @club, status: :created, location: @club }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @club.update_attributes(params[:club])
-        format.html { redirect_to @club, notice: 'Club was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @club.destroy
-
-    respond_to do |format|
-      format.html { redirect_to clubs_url }
-      format.json { head :no_content }
     end
   end
 end
