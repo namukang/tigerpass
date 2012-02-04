03bff7d2c46721f2fe5a8d1a37a223c2f560ac42
Dan Kang <dan@dskang.com>
2012-02-04 18:22:11 -0500

    Use jQuery to change button looks

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,12 +1,27 @@
-$(function() {
-$('.club').hover(function() {
-  // mouse hovers over club
-  $('.club').addClass('faded');
-  $(this).removeClass('faded');
-}, null);
+// Only show events for day
+function filter_events(day) {
+  $('.event').hide();
+  $('.event').filter('.' + day).show();
+}
 
-$('#clubLogos').hover(null, function() {
-  // mouse hovers off club list
-  $('.club').removeClass('faded');
-});
+$(function() {
+  $('.club').hover(function() {
+    // mouse hovers over club
+    $('.club').addClass('faded');
+    $(this).removeClass('faded');
+  }, null);
+
+  $('#clubLogos').hover(null, function() {
+    // mouse hovers off club list
+    $('.club').removeClass('faded');
+  });
+
+  $('#buttons a').click(function(e) {
+    e.preventDefault();
+    $('.currentDayButton').addClass('dayButton');
+    $('.currentDayButton').removeClass('currentDayButton');
+    $(this).children('.button').removeClass('dayButton');
+    $(this).children('.button').addClass('currentDayButton');
+    // filter_events($(this).attr('id'));
+  });
 });
