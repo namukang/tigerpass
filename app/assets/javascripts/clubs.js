e9f9ef178110ae48d7e3659c0083bbc0f4b46457
Dan Kang <dan@dskang.com>
2012-02-04 21:07:57 -0500

    Make logos and buttons all work

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,27 +1,65 @@
-// Only show events for day
-function filter_events(day) {
+var selected_club = null;
+
+// Filter events shown
+function filter_events(filter) {
   $('.event').hide();
-  $('.event').filter('.' + day).show();
+  $('.event').filter('.' + filter).show();
 }
 
 $(function() {
+  // Filter results by default
+  var current_day = $('.currentDayButton').parent().attr('id');
+  filter_events(current_day);
+
   $('.club').hover(function() {
-    // mouse hovers over club
-    $('.club').addClass('faded');
-    $(this).removeClass('faded');
+    if (selected_club === null) {
+      // mouse hovers over club
+      $('.club').addClass('faded');
+      $(this).removeClass('faded');
+    }
   }, null);
 
   $('#clubLogos').hover(null, function() {
-    // mouse hovers off club list
-    $('.club').removeClass('faded');
+    if (selected_club === null) {
+      // mouse hovers off club list
+      $('.club').removeClass('faded');
+    }
   });
 
   $('#buttons a').click(function(e) {
     e.preventDefault();
+    // Make all club logos faded
+    selected_club = null;
+    $('.club').removeClass('faded');
+    // Unpress all buttons
     $('.currentDayButton').addClass('dayButton');
     $('.currentDayButton').removeClass('currentDayButton');
+    // Press this button
     $(this).children('.button').removeClass('dayButton');
     $(this).children('.button').addClass('currentDayButton');
-    // filter_events($(this).attr('id'));
+    // Only show events for this day
+    filter_events($(this).attr('id'));
+  });
+
+  $('#clubLogos a').click(function(e) {
+    e.preventDefault();
+    var selected_logo = $(this).children('div').first();
+    // FIXME: I'm not proud of this
+    var club = selected_logo.attr('class').split(" ")[0];
+    // Deselect club if clicked twice
+    if (club === selected_club) {
+      $('.todayButton').parent().click();
+      return;
+    } else {
+      selected_club = club;
+    }
+    // Fade all clubs except this one
+    $('.club').addClass('faded');
+    selected_logo.removeClass('faded');
+    // Unpress all buttons
+    $('.button').addClass('dayButton');
+    $('.button').removeClass('currentDayButton');
+    // Only show this club's events
+    filter_events(club + "_event")
   });
 });
